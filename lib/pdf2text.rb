70a7e5adc9a80d0d30511cac9d8ddf368d696304
Cliff Wulfman <cwulfman@princeton.edu>
2015-05-14 23:27:17 -0400

    Documents the lib files.

--- lib/pdf2text.rb
+++ lib/pdf2text.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+# This program takes a PDF and converts it to text
+# by running each page through tesseract, writing
+# output to stdout. It takes an optional language
+# parameter to pass into tesseract.
+#
+# Author::   Clifford Wulfman (mailto:cwulfman@princeton.edu)
+
+
 require './pdfconverter.rb'
 require 'optparse'
 
--- lib/pdfconverter.rb
+++ lib/pdfconverter.rb
@@ -1,8 +1,24 @@
 require 'RMagick'
 require 'rtesseract'
 
+# A simple class to run OCR on a PDF file.
+#
+# Author::   Clifford Wulfman (mailto:cwulfman@princeton.edu)
+
 class PDFConverter
 
+  # Initialize an instance with a file name
+  # (file path) and an optional language
+  # parameter, raising an error if no file is provided.
+  # Complete initialization by extracting each
+  # page image and converting it to a resolution
+  # of 200 ppi, which seems optimal for OCR.
+  #
+  # Params:
+  # +fname+:: path to pdf file to be processed
+  # +lang+:: optional language specifier.
+  # TODO: check to make sure lang is in the
+  # set of accepted tesseract language keys.
   def initialize(fname, lang)
     raise ArgumentError, 'no input file' if fname.nil?
     @fname = fname
@@ -12,6 +28,8 @@ class PDFConverter
     end
   end
 
+  # Iterate over the images in the object,
+  # running each through tesseract.
   def to_s
     @images.each do |img|
       tess = RTesseract.new('', :lang => @lang)
