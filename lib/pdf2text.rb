62a7febc5b2ac7281b5adf6710e4703c60f21b00
Cliff Wulfman <cwulfman@princeton.edu>
2015-05-14 22:57:01 -0400

    Adds support for passing in language parameter.

--- lib/pdf2text.rb
+++ lib/pdf2text.rb
@@ -1,11 +1,24 @@
 #!/usr/bin/env ruby
 
 require './pdfconverter.rb'
+require 'optparse'
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = 'Usage: pdf2text filename -l language'
+  opts.on('-l', '--lang lang', 'language') do |lang|
+    options[:lang] = lang
+  end
+end
+
+optparse.parse!
 
 fname = ARGV[0]
+lang = options[:lang] ? options[:lang] : 'eng'
 raise ArgumentError, "no input file" if fname.nil?
 
 raise ArgumentError, "file doesn't exist" unless File.file?(fname)
 
-converter = PDFConverter.new(fname)
+converter = PDFConverter.new(fname, lang)
 converter.to_s
--- lib/pdfconverter.rb
+++ lib/pdfconverter.rb
@@ -3,9 +3,10 @@ require 'rtesseract'
 
 class PDFConverter
 
-  def initialize(fname)
+  def initialize(fname, lang)
     raise ArgumentError, 'no input file' if fname.nil?
     @fname = fname
+    @lang  = lang
     @images = Magick::ImageList.new(fname) do
       self.density = '200'
     end
@@ -13,7 +14,7 @@ class PDFConverter
 
   def to_s
     @images.each do |img|
-      tess = RTesseract.new('')
+      tess = RTesseract.new('', :lang => @lang)
       tess.from_blob(img.to_blob {self.format = 'jpg';
                        self.depth = 8;
                        self.quality = 95 } )
